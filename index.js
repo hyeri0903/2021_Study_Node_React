51a68dcb267094b631785dae22cf852b864fd34f
hyeri <kbi09@naver.com>
2021-01-24 00:30:48 +0900

    upload login function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require("body-parser");
-
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 
@@ -13,7 +13,7 @@ const {User} = require("./models/User");
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json -> json 현태 데이터를 분석해서 가져올 수 있게 함
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 /* application과 mongodb 연동*/
 const mongoose = require('mongoose')
@@ -42,6 +42,38 @@ app.post('/register', (req, res) => {
 
 	})
 })
+
+app.post('/login', (req, res) => {
+	//1.요청된 이메일을 디비에서 존재하는지 찾음
+	User.findOne({email : req.body.email}, (err, user) => {
+		if(!user){
+			return res.json({
+				loginSuccess : false,
+				message: "입력한 이메일에 해당하는 유저가 없습니다."
+			})
+		}
+		//2.존재하면 비밀번호 일치 여부 확인
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if(!isMatch) 
+				return res.json({loginSuccess : false, message: "비밀번호가 틀렸습니다."})
+
+			//3.일치하면 token 생성
+			user.generateToken((err, user)=> {
+				if(err) return res.status(400).send(err);
+
+				// token을 저장한다. where? 쿠키, 로컬스토리지 등 , 여기선 쿠키에 저장
+				res.cookie("x_auth", user.token)
+				.status(200)
+				.json({ loginSuccess: true, userId: user._id})
+
+			})
+		})
+
+	})
+
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //const bcrypt = require('bcrypt');
 //const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
 	name: {
@@ -49,9 +50,38 @@ userSchema.pre('save', function(next){
 		    	next()
 			})
 		})
+	}else{
+		next()
 	}
 })
 */
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+	//비밀번호 비교 함수, (원래 비번, 콜백 함수)
+	//plainpassword를 암호화하여 비교
+	/*
+	bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+		if(err) return cb(err),
+		cb(null, isMatch)
+	})
+	*/
+	userSchema.compare(plainPassword, this.password, function(err, isMatch) {
+		if(err) return cb(err),
+		cb(null, isMatch)
+	})
+}
+
+userSchema.methods.generateToken = function(cb){
+	var user = this;
+	//jsonwebtoken 을 이용하여 token 생성
+	var token = jwt.sign(user._id.toHexString(), , 'secretToken')
+	//token = user._id + 'secretToken'
+	user.token = token
+	user.save(function(err, user) {
+		if(err) return cb(err)
+			cb(null, user)
+	})
+}
 
 const User = mongoose.model('User', userSchema)
 
